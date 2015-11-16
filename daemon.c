2bcbfbd3cfd1ba13bc3f9e5a275d228656c39134
think <microhuang@live.com>
2015-11-17 00:17:19 +0800

    Create daemon.c

---
+++ daemon.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+int main(void)
+{
+        pid_t pid,i;
+        if((pid=fork())<0)
+        {
+                perror("fork");return -1;
+        }else if(pid!=0)
+        {
+                exit(0); //结束父进程
+        }
+        //子进程
+        setsid(); //建立新回话期
+        chdir("/tmp"); //改变运行根目录
+        umask(0); //设置屏蔽字
+        for(i=0;i<sysconf(_SC_OPEN_MAX);i++)
+        {
+                close(i);//关闭父进程打开的文件描述符
+        }
+        //todo
+        return 0;
+}
